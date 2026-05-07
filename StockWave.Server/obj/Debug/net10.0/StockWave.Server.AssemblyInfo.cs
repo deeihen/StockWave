d2c2a5233f1b062afa5f45c8e61339baf76611a6
deeihen <markdennisconcha14@gmail.com>
2026-05-07 19:47:44 +0800

    resolved conflict

--- StockWave.Server/obj/Debug/net10.0/StockWave.Server.AssemblyInfo.cs
+++ StockWave.Server/obj/Debug/net10.0/StockWave.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StockWave.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8ed2e0f1e4033fe1a84217dbed55a2899e932a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b193237148ff09abee3ded331fa41fde1cfca56")]
 [assembly: System.Reflection.AssemblyProductAttribute("StockWave.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StockWave.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
